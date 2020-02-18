c22f3868e59125f781d297174afb1538ef65a352
Evandro.J <juniok591@gmai.com>
2020-02-18 17:19:56 -0300

    library

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,12 @@
+export default {
+    title: 'dark',
+
+    colors: {
+        primary: '#1C5D99',
+        secundary: '#639FAB',
+
+        background: '#222',
+        text: '#fff',
+    },
+};
+
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,12 @@
+
+export default {
+    title: 'light',
+  
+    colors: {
+      primary: '#1C5D99',
+      secundary: '#639FAB',
+  
+      background: '#f5f5f5',
+      text: '#333'
+    },
+  };
